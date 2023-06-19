a75c4768f0382c6c438a95aca2fc55a1823aa253
adamdasovich <adamdasovich@gmail.com>
2023-06-19 14:40:18 -0400

    completed product grid page

--- src/pages/productList/ProductList.jsx
+++ src/pages/productList/ProductList.jsx
@@ -11,7 +11,35 @@ const ProductList = () => {
 	  const handleDelete = (id) => {
 		setData(data.filter((item) => item.id !== id));
 	  };
-	  
+
+	  const columns = [
+		{ field: 'id', headerName: 'ID', width: 150 },
+		{ field: 'product', headerName: 'Product', width: 310, renderCell: (params) => {
+			return (
+				<div className='productListItem'>
+					<img className='productListImage' src={params.row.img} alt='' />
+					{params.row.name}
+				</div>
+			)
+		} },
+		{ field: 'stock', headerName: 'Stock', width: 200 },
+		{ field: 'status', headerName: 'Status', width: 200 },
+		{ field: 'price', headerName: 'Price', width: 200 },
+		{ field: 'action', headerName: 'Action', width: 200, renderCell: (params) => {
+			return (
+				<>
+					<Link to={'/product/' + params.row.id}>
+						<button className='productListEdit'>Edit</button>
+					</Link>
+					<DeleteOutlineIcon
+						className='productListDelete'
+						onClick={() => handleDelete(params.row.id)}
+					/>
+				</>
+			)
+		} },
+	];
+
   return (
 	<div className='productList' >
 		<DataGrid 
